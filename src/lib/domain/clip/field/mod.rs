4ec03dde97c3b3d16d0ed5b9547fce0b3a67c26d
nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>
2024-08-30 23:42:23 +0530

    Add clip struct with respective modules and dependencies

---
+++ src/lib/domain/clip/field/mod.rs
@@ -0,0 +1,17 @@
+pub use clip_id::ClipId;
+pub use content::Content;
+pub use expires::Expires;
+pub use hits::Hits;
+pub use password::Password;
+pub use posted::Posted;
+pub use shortcode::ShortCode;
+pub use title::Title;
+
+mod clip_id;
+mod content;
+mod shortcode;
+mod title;
+mod posted;
+mod expires;
+mod password;
+mod hits;
