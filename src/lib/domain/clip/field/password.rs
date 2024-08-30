8f79875eda865f7ed95d0240d4eae3957546847e
nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>
2024-08-31 04:00:31 +0530

    Add password struct and impl

---
+++ src/lib/domain/clip/field/password.rs
@@ -0,0 +1,47 @@
+use std::str::FromStr;
+
+use serde::{Deserialize, Serialize};
+
+use crate::domain::clip::ClipError;
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, PartialOrd)]
+pub struct Password(Option<String>);
+
+impl Password {
+    const MIN_LENGTH: usize = 8;
+    pub fn new<T: Into<Option<String>>>(password: T) -> Result<Self, ClipError> {
+        let password: Option<String> = password.into();
+        match password {
+            Some(password) => {
+                let trimmed_password = password.trim();
+                if trimmed_password.is_empty() || trimmed_password.len() < Self::MIN_LENGTH {
+                    Ok(Self(None))
+                } else {
+                    Ok(Self(Some(trimmed_password.to_string())))
+                }
+            }
+            None => Ok(Self(None)),
+        }
+    }
+
+    pub fn into_inner(self) -> Option<String> {
+        self.0
+    }
+
+    pub fn has_password(&self) -> bool {
+        self.0.is_some()
+    }
+}
+
+impl Default for Password {
+    fn default() -> Self {
+        Self(None)
+    }
+}
+
+impl FromStr for Password {
+    type Err = ClipError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::new(s.to_string())
+    }
+}
