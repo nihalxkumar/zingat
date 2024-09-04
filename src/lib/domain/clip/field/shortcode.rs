97c8ae4772f37bf80da2e904f53cba1a5cbcc3ac
Nihal  Kumar <121309701+nihalxkumar@users.noreply.github.com>
2024-09-04 11:25:08 +0530

    Add ShortCode struct with random generation and conversion implementations
    Signed-off-by: nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>

---
+++ src/lib/domain/clip/field/shortcode.rs
@@ -0,0 +1,66 @@
+use std::str::FromStr;
+
+use derive_more::From;
+use serde::{Deserialize, Serialize};
+
+use crate::domain::clip::ClipError;
+
+#[derive(Debug, Clone, Deserialize, Serialize, From)]
+pub struct ShortCode(String);
+
+impl ShortCode {
+    pub fn new() -> Self {
+        use rand::prelude::*;
+        let allowed_chars = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '2', '3',
+            '4', '5', '6', '7', '8', '9',
+        ];
+
+        let mut rng = thread_rng();
+        let mut shortcode = String::with_capacity(10);
+        for _ in 0..10 {
+            shortcode.push(
+                *allowed_chars
+                    .choose(&mut rng)
+                    .expect("No character found in allowed_chars array"),
+            );
+        }
+        Self(shortcode)
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+}
+
+/// The Default implementation is a new randomly generated shortcode.
+impl Default for ShortCode {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl From<ShortCode> for String {
+    fn from(shortcode: ShortCode) -> Self {
+        shortcode.0
+    }
+}
+
+impl From<&str> for ShortCode {
+    fn from(shortcode: &str) -> Self {
+        ShortCode(shortcode.to_owned())
+    }
+}
+
+impl FromStr for ShortCode {
+    type Err = ClipError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.into()))
+    }
+}
