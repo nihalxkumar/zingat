17a9eff7e017810e806b60f3516bd83546b24dc1
nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>
2024-09-14 23:39:29 +0530

    Add ClipID struct with implementations
    Signed-off-by: nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>

---
+++ src/lib/domain/clip/field/clip_id.rs
@@ -0,0 +1,26 @@
+use derive_more::Constructor;
+use serde::{Deserialize, Serialize};
+
+use crate::data::DbId;
+use crate::domain::clip::Clip;
+
+#[derive(Clone, Debug, Constructor, Deserialize, Serialize)]
+pub struct ClipId(DbId);
+
+impl ClipId {
+    pub fn into_inner(self) -> DbId {
+        self.0
+    }
+}
+
+impl From<DbId> for ClipId {
+    fn from(id: DbId) -> Self {
+        Self::new(id)
+    }
+}
+
+impl Default for ClipId {
+    fn default() -> Self {
+        Self::new(DbId::new())
+    }
+}
