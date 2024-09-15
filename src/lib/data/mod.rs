13da50c3035e3bb9f1fc33b443ccb1fb3195afe4
nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>
2024-09-15 22:36:17 +0530

    Add sqlx typealiases, dependencies and DataError
    Signed-off-by: nihalxkumar <121309701+nihalxkumar@users.noreply.github.com>

--- src/lib/data/mod.rs
+++ src/lib/data/mod.rs
@@ -2,7 +2,20 @@ use std::str::FromStr;
 
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
+use sqlx::Sqlite;
 use uuid::Uuid;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::sqlite::SqlitePool;
+pub type Transaction<'t> = sqlx::sqlite::SqliteRow;
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
 
 #[derive(Clone, Debug, From, Display, Deserialize, Serialize)]
 pub struct DbId(Uuid);
