25ce02fad7571d387e2b90b4afd5f787373abef2
Nihal Kumar <121309701+nihalxkumar@users.noreply.github.com>
2025-01-01 05:14:24 +0530

    Add PageContext trait and implement it for Home.

---
+++ src/lib/web/ctx.rs
@@ -0,0 +1,53 @@
+use derive_more::Constructor;
+use serde::Serialize;
+
+pub trait PageContext {
+    fn title(&self) -> &str;
+    fn template_path(&self) -> &str;
+    fn parent(&self) -> &str;
+}
+
+
+#[derive(Debug, Serialize)]
+pub struct Home{}
+
+impl Default for Home {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl PageContext for Home {
+    fn title(&self) -> &str {
+        "Zingat - pastebin in rs"
+    }
+    fn template_path(&self) -> &str {
+        "home"
+    }
+    fn parent(&self) -> &str {
+        "base"
+    }
+}
+
+#[derive(Debug, Serialize, Constructor)]
+pub struct ViewClip {
+    pub clip: crate::Clip
+}
+
+#[derive(Debug, Serialize, Constructor)]
+pub struct PasswordRequired {
+    shortcode: crate::ShortCode,
+    
+}
+
+impl PageContext for PasswordRequired {
+    fn title(&self) -> &str {
+        "Password Required"
+    }
+    fn template_path(&self) -> &str {
+        "clip_need_password"
+    }
+    fn parent(&self) -> &str {
+        "base"
+    }
+}
