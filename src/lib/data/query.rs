4576d5d338bc7f991b27a90f77cb942b91de0de9
Nihal Kumar <121309701+nihalxkumar@users.noreply.github.com>
2025-01-11 23:59:24 +0530

    Implements API key functionality and routes.

--- src/lib/data/query.rs
+++ src/lib/data/query.rs
@@ -1,7 +1,9 @@
+use rocket::http::ext::IntoCollection;
 use super::model;
 use crate::data::{DataError, DatabasePool};
 use sqlx::Row;
 use crate::ShortCode;
+use crate::web::api::ApiKey;
 
 type Result<T> = std::result::Result<T, DataError>;
 
@@ -71,4 +73,45 @@ pub async fn update_clip<M: Into<model::UpdateClip>>(model: M, pool: &DatabasePo
         .await?;
     get_clip(model.shortcode,pool).await
 
+}
+
+pub async fn save_api_key (api_key: ApiKey, pool: &DatabasePool) -> Result<ApiKey> {
+    let bytes = api_key.clone().into_inner();
+    let _ = sqlx::query!( "INSERT INTO api_keys (api_key) VALUES (?)", bytes )
+        .execute(pool)
+        .await
+        .map(|_| ())?;
+    Ok(api_key)
+}
+
+pub enum RevocationStatus{
+    Revoked,
+    NotFound,
+}
+
+pub async fn revoke_api_key(api_key: ApiKey, pool: &DatabasePool) -> Result<RevocationStatus> {
+    let bytes = api_key.clone().into_inner();
+    Ok(
+        sqlx::query!( "DELETE FROM api_keys WHERE api_key == ?", bytes )
+            .execute(pool)
+            .await
+            .map(|result| match result.rows_affected(){
+                0 => RevocationStatus::NotFound,
+                _ => RevocationStatus::Revoked
+            })?,
+    )
+}
+
+pub async fn api_key_in_valid(api_key: ApiKey, pool: &DatabasePool) -> Result<bool> {
+    let bytes = api_key.clone().into_inner();
+    Ok(
+        sqlx::query("SELECT COUNT(api_key) FROM api_keys WHERE api_key = ?")
+            .bind( bytes )
+            .fetch_one(pool)
+            .await
+            .map(|row| {
+                let count: u32 = row.get(0);
+                count > 0
+            })?,
+    )
 }
